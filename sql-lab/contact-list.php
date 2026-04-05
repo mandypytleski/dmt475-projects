5ee14508c67a2d31cc6146ae1f00d9ac623bcbf0
Mandy Pytleski <pytleskia5362@my.uwstout.edu>
2026-04-05 11:10:05 -0500

    got everything working.

--- sql-lab/contact-list.php
+++ sql-lab/contact-list.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("../../db_connect.php");
+    require_once("../db_connect.php");
 
     $con = mysqli_connect($servername, $db_username, $db_password, $db_name);
 
@@ -7,11 +7,20 @@
     if(!$con){
         die("Connection failed: " .mysqli_connect_error());
     }
+
+    //Pull information from the data table
+    $sql = "SELECT first_name, last_name, email_address from contact_info";
+    $result = mysqli_query($con, $sql);
+
+    //Check if access to table was successful
+    if(!$result){
+        die("Access to table failed: " .mysqli_error($con));
+    }
+
+    else{
+        echo "<h1>Friends of Mine</h1>\n";
+        while($item = mysqli_fetch_array($result)){
+            echo "<p><strong>Name:</strong> " . $item["first_name"] . " " . $item["last_name"] . "<br>\n<strong>Email: </strong><a href='mailto:" . $item["email_address"] . "'>" . $item["email_address"] . "</a></p>";
+        }
+    }
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf8-">
-        <title></title>
-    </head>
-</html>
